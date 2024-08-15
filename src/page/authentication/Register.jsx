44ef72382c5cf488fdb16e3375eb8781e4b30483
HIMU005 <hashanuzzaman99@gamil.com>
2024-08-15 12:27:46 +0600

    register user part done

--- src/page/authentication/Register.jsx
+++ src/page/authentication/Register.jsx
@@ -1,8 +1,40 @@
-
+import { Link } from 'react-router-dom';
+import './register.css'
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../Firebase/firebase.config';
+import { toast } from 'react-toastify';
 const Register = () => {
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const form = e.target;
+        const username = form.userName.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(username, email, password);
+
+        const newUser = { email, password, displayName: username };
+        console.log(newUser);
+        try {
+            createUserWithEmailAndPassword(auth, email, password);
+            toast.success('user created');
+        } catch (error) {
+            toast.error(error.message);
+        }
+    }
     return (
         <div>
-            register
+            <form onSubmit={handleSubmit} className="flex flex-col gap-3 bg-white rounded-2xl px-7 py-5">
+                <span className="shadow-lg">Register to join out family</span>
+                {/* <input name='' id="file" type="file" /> */}
+                <label htmlFor=""></label>
+                <label htmlFor="userName">Username</label>
+                <input name='userName' type="text" className="border-0 outline-none shadow-xl px-4 py-2 w-full focus:border-2 focus:border-solid focus:shadow-xl focus:rounded-2xl" placeholder="username" />
+                <input name='email' type="email" className="border-0 outline-none shadow-xl px-4 py-2 w-full focus:border-2 focus:border-solid focus:shadow-xl focus:rounded-2xl" placeholder="email" />
+                <input name='password' type="password" className="border-0 outline-none shadow-xl px-4 py-2 w-full focus:border-2 focus:border-solid focus:shadow-xl focus:rounded-2xl" placeholder="password" />
+                <span className="">Already have an account ? <Link className='btn btn-link'>Sign in</Link>
+                </span>
+                <input className='btn btn-outline btn-info' type='submit' value='Register' />
+            </form>
         </div>
     );
 };
