905a21ecf075985c1780fd25ffacdb46cf644d53
HIMU005 <hashanuzzaman99@gamil.com>
2024-08-16 19:42:49 +0600

    product card redesign

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,43 +1,26 @@
+/* eslint-disable react/prop-types */
 import './ProductCard.css'
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
+    const { productName, description, ratings, price, creationDateTime, productImage } = product;
     return (
-        /* From Uiverse.io by ElSombrero2 */
-        <div className="card">
-            <div className="content">
-                <div className="back">
-                    <div className="back-content">
-                        <img src="https://i.ibb.co/7yQ5YCC/electronics.jpg" alt="" />
-                        <strong>productName</strong>
-                    </div>
-                </div>
-                <div className="front">
 
-                    <div className="img">
-                        <div className="circle">
-                        </div>
-                        <div className="circle right" id="">
-                        </div>
-                        <div className="circle bottom" id="">
-                        </div>
-                    </div>
 
-                    <div className="front-content">
-                        <small className="badge text-white">productName</small>
-                        <div className="description">
-                            <div className="title">
-                                <p className="title ">
-                                    <s className='text-white font-normal'>description</s>
-                                </p>
-                                {/* <svg fill-rule="nonzero" height="15px" width="15px" viewBox="0,0,256,256" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g style="mix-blend-mode: normal" text-anchor="none" font-size="none" font-weight="none" font-family="none" stroke-dashoffset="0" stroke-dasharray="" stroke-miterlimit="10" stroke-linejoin="miter" stroke-linecap="butt" stroke-width="1" stroke="none" fill-rule="nonzero" fill="#20c997"><g transform="scale(8,8)"><path d="M25,27l-9,-6.75l-9,6.75v-23h18z"></path></g></g></svg> */}
-                            </div>
-                            <p className="card-footer">
-                                rating: ratings | price: price|  date: creationDateTime
-                            </p>
-                        </div>
-                    </div>
-                </div>
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow p-8">
+            <a href="#">
+                <img className="rounded-t-lg" src={productImage} alt="" />
+            </a>
+            <div className="p-5">
+                <a href="#">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight">{productName}</h5>
+                </a>
+                <p className="mb-3 font-normal ">{description}</p>
+                <p>rating: <span className='font-bold'>{ratings}</span> </p>
+                <p>price: <span className='font-bold'>{price}</span> </p>
+                <p>date: <span className='font-bold'>{creationDateTime}</span> </p>
             </div>
         </div>
+
+
     );
 };
 
--- src/page/Products.jsx
+++ src/page/Products.jsx
@@ -9,22 +9,38 @@ const Products = () => {
     const [categories, setCategories] = useState([]);
     const [singleBrand, setSingleBrand] = useState('');
     const [singleCategory, setSingleCategory] = useState('');
+    const [total, setTotal] = useState(0);
+    const [page, setPage] = useState(1);
     const [min, setMin] = useState(1);
     const [max, setMax] = useState(2000);
+    const [sorted, setSorted] = useState('');
     const axiosCommon = useAxiosCommon();
+    const perPage = 10;     //one page have 10 cards
+    const totalPage = Math.ceil(total.count / perPage);
+    // console.log(totalPage);
 
     useEffect(() => {
         loadData();
         loadBrands();
         loadCategory();
         check();
-    }, [min, max])
+        loadProductNumber();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [min, max, page, singleBrand, singleCategory])
     const check = () => {
         if (min > max) {
             toast.error("Min price can't be greater than Max price")
         }
     }
 
+    const loadProductNumber = async () => {
+        try {
+            const { data } = await axiosCommon.get('/productCount');
+            setTotal(data);
+        } catch (error) {
+            console.error("Error fetching products:", error);
+        }
+    };
 
     const loadData = async () => {
         try {
@@ -81,13 +97,19 @@ const Products = () => {
 
     // apply all the document 
     const handleClick = async () => {
-        const search = { max, min, singleBrand, singleCategory }
+        const search = { max, min, singleBrand, singleCategory, page, perPage, sorted }
         try {
             const { data } = await axiosCommon.get('/products', { params: search });
             setProducts(data);
         } catch (error) {
             console.error("Error fetching filtered products:", error);
         }
+    }
+
+    // select sort item menu
+    const handleSort = e => {
+        const sort = e.target.value;
+        setSorted(sort);
     }
 
 
@@ -99,31 +121,57 @@ const Products = () => {
 
     return (
         <div>
-            {/* category input  */}
-            <select onChange={handleCategory} name="" id="">
-                <option value="">Select Category</option>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                {/* category input  */}
+                <select className="border border-primary px-4 py-2 rounded-xl" onChange={handleCategory} name="" id="">
+                    <option value="">Select Category</option>
+                    {
+                        categories.map(category =>
+                            <option key={category._id} value={category.category}>{category.category}</option>
+                        )
+                    }
+                </select>
+                {/* brand input */}
+                <select className="border border-primary px-4 py-2 rounded-xl" onChange={handleBrand} name="" id="">
+                    <option value="">Select Brand</option>
+                    {
+                        brands.map(brand =>
+                            <option key={brand._id} value={brand.brand}>{brand.brand}</option>
+                        )
+                    }
+                </select>
+
+                <select className="border border-primary px-4 py-2 rounded-xl" onChange={handleSort} name="" id="">
+                    <option value="">Select sort by</option>
+                    <option value="lowToHigh">Low to high</option>
+                    <option value="HighToLow">High to Low</option>
+                    <option value="Newest">Newest</option>
+
+                </select>
+
+                <input className="border border-primary px-4 py-2 rounded-xl" onChange={handleMin} type="number" name="min" id="" placeholder="Min price" defaultValue={1} />
+                <input className="border border-primary px-4 py-2 rounded-xl" onChange={handleMax} type="number" name="max" id="" placeholder="Max price" defaultValue={2000} />
+
+                <button onClick={handleClick} className="btn btn-outline btn-primary w-auto">Apply</button>
+            </div>
+
+            <div className="flex justify-normal pb-6">
+                <button onClick={() => { setPage(page - 1) }} disabled={page === 1} className="w-20 btn ">«</button>
+                <button className="w-20 btn btn-active">page {page}</button>
+                <button onClick={() => { setPage(page + 1) }} disabled={page === totalPage} className="w-20 btn ">»</button>
+            </div>
+
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
                 {
-                    categories.map(category =>
-                        <option key={category._id} value={category.category}>{category.category}</option>
+                    products.map(product =>
+                        <ProductCard key={product._id} product={product} />
                     )
                 }
-            </select>
-            {/* brand input */}
-            <select onChange={handleBrand} name="" id="">
-                <option value="">Select Brand</option>
-                {
-                    brands.map(brand =>
-                        <option key={brand._id} value={brand.brand}>{brand.brand}</option>
-                    )
-                }
-            </select>
+            </div>
 
-            <input onChange={handleMin} type="number" name="min" id="" placeholder="Min price" defaultValue={1} />
-            <input onChange={handleMax} type="number" name="max" id="" placeholder="Max price" defaultValue={2000} />
+            {/* <ProductCard /> */}
 
-            <button onClick={handleClick} className="btn btn-outline btn-primary w-auto">Apply</button>
-
-            <ProductCard />
         </div>
     );
 };
